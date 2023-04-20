5321928598b5f5aa7f657be16e0a0220d21dcfa1
osb <ton97917@gmail.com>
2023-04-20 18:34:02 +0900

    navigator추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_carrot/detail_page.dart';
+
+import 'carrot_item.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,80 +14,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     items.add(Carrotitem(title: 'ㅇㅇㅇㅇㅇ', address: '학굔데요?', price: 1000));
-    items.add(Carrotitem(title: 'ㅇㅇㅇㅇㅇ', address: '학굔데요?', price: 1000));
-    items.add(Carrotitem(title: 'ㅇㅇㅇㅇㅇ', address: '학굔데요?', price: 1000));
-    items.add(Carrotitem(title: 'ㅇㅇㅇㅇㅇ', address: '학굔데요?', price: 1000));
-    items.add(Carrotitem(title: 'ㅇㅇㅇㅇㅇ', address: '학굔데요?', price: 1000));
-    items.add(Carrotitem(title: 'ㅇㅇㅇㅇㅇ', address: '학굔데요?', price: 1000));
-    items.add(Carrotitem(title: 'ㅇㅇㅇㅇㅇ', address: '학굔데요?', price: 1000));
+    items.add(Carrotitem(title: 'ㅇㅇㅇㅇㅇ', address: '학굔데요?', price: 2000));
+    items.add(Carrotitem(title: 'ㅇㅇㅇㅇㅇ', address: '학굔데요?', price: 3000));
+    items.add(Carrotitem(title: 'ㅇㅇㅇㅇㅇ', address: '학굔데요?', price: 4000));
+    items.add(Carrotitem(title: 'ㅇㅇㅇㅇㅇ', address: '학굔데요?', price: 5000));
+    items.add(Carrotitem(title: 'ㅇㅇㅇㅇㅇ', address: '학굔데요?', price: 6000));
+    items.add(Carrotitem(title: 'ㅇㅇㅇㅇㅇ', address: '학굔데요?', price: 7000));
 
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('malbob narket'),
-            backgroundColor: Colors.amber,
-          ),
-          body: SingleChildScrollView(
-            child: Column(
-              children: [for (var item in items) item],
-            ),
-          )),
+      home: HomePage(items: items),
     );
   }
 }
 
-class Carrotitem extends StatelessWidget {
-  String title, address;
-  int price;
-  Carrotitem({
-    required this.title,
-    required this.address,
-    required this.price,
+class HomePage extends StatelessWidget {
+  const HomePage({
     super.key,
+    required this.items,
   });
+
+  final List<Carrotitem> items;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.max,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          width: 150,
-          height: 150,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              image:
-                  const DecorationImage(image: AssetImage('imges/12312.jpg'))),
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('malbob narket'),
+          backgroundColor: Colors.amber,
         ),
-        Flexible(
-          flex: 1,
-          child: SizedBox(
-            height: 150,
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  address,
-                  style: const TextStyle(decoration: TextDecoration.underline),
-                ),
-                Text('$price'),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: const [Icon(Icons.heart_broken), Text('12')],
-                )
-              ],
-            ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              for (var item in items)
+                GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailPage(price: item.price),
+                          ));
+                    },
+                    child: item)
+            ],
           ),
-        )
-      ],
-    );
+        ));
   }
 }
